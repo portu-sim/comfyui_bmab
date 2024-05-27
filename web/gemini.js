de3ecdd769c3ddc2a1ea33e8413820b58dee1c98
portu.sim <portu.sim.kr@gmail.com>
2024-05-28 07:27:04 +0900

    * New Node : Preview Text

--- web/gemini.js
+++ web/gemini.js
@@ -8,12 +8,10 @@ app.registerExtension({
 
             const onExecuted = nodeType.prototype.onExecuted;
             nodeType.prototype.onExecuted = function (texts) {
-                console.log(`on executed ${texts}`);
                 onExecuted?.apply(this, arguments);
                 let widget_id = this?.widgets.findIndex(
                     obj => obj.name === 'random_seed'
                 );
-                console.log(`on set ${texts?.string}`);
                 this.widgets[widget_id].value = Number(texts?.string)
                 app.graph.setDirtyCanvas(true);
             };
